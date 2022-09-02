d3c2addfd2a41e8583041190348595d70829931d
Kacper Stojek <kacper.stojek@3mdeb.com>
2022-09-02 08:11:43 +0200

    xen/arch/x86: reserve TXT memory
    TXT heap is marked as reserved in e820 to protect against being allocated
    and overwritten.
    
    Signed-off-by: Kacper Stojek <kacper.stojek@3mdeb.com>
    Signed-off-by: Krystian Hebel <krystian.hebel@3mdeb.com>
    Signed-off-by: Sergii Dmytruk <sergii.dmytruk@3mdeb.com>

--- xen/arch/x86/setup.c
+++ xen/arch/x86/setup.c
@@ -59,6 +59,7 @@
 #include <asm/microcode.h>
 #include <asm/prot-key.h>
 #include <asm/pv/domain.h>
+#include <asm/intel_txt.h>
 
 /* opt_nosmp: If true, secondary processors are ignored. */
 static bool __initdata opt_nosmp;
@@ -978,9 +979,6 @@ static struct domain *__init create_dom0(const module_t *image,
     return d;
 }
 
-/* How much of the directmap is prebuilt at compile time. */
-#define PREBUILT_MAP_LIMIT (1 << L2_PAGETABLE_SHIFT)
-
 void asmlinkage __init noreturn __start_xen(unsigned long mbi_p)
 {
     const char *memmap_type = NULL, *loader, *cmdline = "";
@@ -1296,6 +1294,14 @@ void asmlinkage __init noreturn __start_xen(unsigned long mbi_p)
             printk("  - Disabling PV32 due to CET\n");
         }
 #endif
+    }
+
+    if ( slaunch_active )
+    {
+        /* Prepare for TXT-related code. */
+        map_txt_mem_regions();
+        /* Reserve TXT heap and SINIT. */
+        protect_txt_mem_regions();
     }
 
     /* Sanitise the raw E820 map to produce a final clean version. */
