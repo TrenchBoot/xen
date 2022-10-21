5439298c641e158c392a8eef48b1adc472ab0fd2
Krystian Hebel <krystian.hebel@3mdeb.com>
2022-10-21 18:46:33 +0200

    x86/slaunch: measure MBI into TPM
    Make slaunch-tpm.c compile in early boot environment use it to measure
    MBI in early 32b code without paging (gets triggered from head.S).
    
    The fact of the measurement is not yet stored anywhere as there is no
    code for TPM event log discovery and modification.
    
    Signed-off-by: Krystian Hebel <krystian.hebel@3mdeb.com>
    Signed-off-by: Sergii Dmytruk <sergii.dmytruk@3mdeb.com>

--- xen/arch/x86/slaunch.c
+++ xen/arch/x86/slaunch.c
@@ -18,6 +18,7 @@
 #include <asm/setup.h>
 #include <asm/slaunch.h>
 #include <asm/slaunch-tpm.h>
+#include <asm/tpm.h>
 
 /*
  * These variables are assigned to by the code near Xen's entry point.
@@ -77,6 +78,9 @@ void __init slaunch_map_mem_regions(void)
     int rc;
     paddr_t evt_log_addr;
     uint32_t evt_log_size;
+
+    rc = slaunch_map_l2(TPM_MMIO_BASE, TPM_MMIO_SIZE);
+    BUG_ON(rc != 0);
 
     /* Vendor-specific part. */
     txt_map_mem_regions();
