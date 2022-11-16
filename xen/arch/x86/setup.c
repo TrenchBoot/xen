f93d0825139c994bccbbdd323f59a0443bb99b7c
Krystian Hebel <krystian.hebel@3mdeb.com>
2022-11-16 15:03:07 +0100

    x86/boot: choose AP stack based on APIC ID
    This is made as the first step of making parallel AP bring-up possible.
    It should be enough for pre-C code.
    
    Parallel AP bring-up is necessary because TXT by design releases all APs
    at once. In addition to that it reduces number of IPIs (and more
    importantly, delays between them) required to start all logical
    processors. This results in significant reduction of boot time, even
    when DRTM is not used, with performance gain growing with the number of
    logical CPUs.
    
    Signed-off-by: Krystian Hebel <krystian.hebel@3mdeb.com>
    Signed-off-by: Sergii Dmytruk <sergii.dmytruk@3mdeb.com>

--- xen/arch/x86/setup.c
+++ xen/arch/x86/setup.c
@@ -2141,6 +2141,7 @@ void asmlinkage __init noreturn __start_xen(void)
      */
     if ( !pv_shim )
     {
+        /* Separate loop to make parallel AP bringup possible. */
         for_each_present_cpu ( i )
         {
             /* Set up cpu_to_node[]. */
@@ -2148,6 +2149,12 @@ void asmlinkage __init noreturn __start_xen(void)
             /* Set up node_to_cpumask based on cpu_to_node[]. */
             numa_add_cpu(i);
 
+            if ( stack_base[i] == NULL )
+                stack_base[i] = cpu_alloc_stack(i);
+        }
+
+        for_each_present_cpu ( i )
+        {
             if ( (park_offline_cpus || num_online_cpus() < max_cpus) &&
                  !cpu_online(i) )
             {
