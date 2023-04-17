69a63e5b660c653118fe845fe86cfad5fc832805
Krystian Hebel <krystian.hebel@3mdeb.com>
2023-04-17 20:10:13 +0200

    x86/include/asm/intel-txt.h: constants and accessors for TXT registers and heap
    The file contains base address of TXT register spaces, offsets of
    registers within them, error codes and inline functions for accessing
    structures stored on TXT heap.
    
    xen/arch/x86/tboot.c is updated to use definitions from this new header
    instead of duplicating them.  The change in tboot_protect_mem_regions()
    there is caused by going from NR_TXT_CONFIG_PAGES to
    TXT_CONFIG_SPACE_SIZE which avoids multiplying number of pages by page
    size on every use.
    
    Signed-off-by: Krystian Hebel <krystian.hebel@3mdeb.com>
    Signed-off-by: Sergii Dmytruk <sergii.dmytruk@3mdeb.com>

--- xen/arch/x86/tboot.c
+++ xen/arch/x86/tboot.c
@@ -17,6 +17,7 @@
 #include <asm/setup.h>
 #include <asm/tboot.h>
 #include <asm/trampoline.h>
+#include <asm/intel-txt.h>
 
 #include <crypto/vmac.h>
 
@@ -36,23 +37,6 @@ static uint64_t __initdata txt_heap_base, __initdata txt_heap_size;
 static uint64_t __initdata sinit_base, __initdata sinit_size;
 
 static bool __ro_after_init is_vtd;
-
-/*
- * TXT configuration registers (offsets from TXT_{PUB, PRIV}_CONFIG_REGS_BASE)
- */
-
-#define TXT_PUB_CONFIG_REGS_BASE       0xfed30000U
-#define TXT_PRIV_CONFIG_REGS_BASE      0xfed20000U
-
-/* # pages for each config regs space - used by fixmap */
-#define NR_TXT_CONFIG_PAGES     ((TXT_PUB_CONFIG_REGS_BASE -                \
-                                  TXT_PRIV_CONFIG_REGS_BASE) >> PAGE_SHIFT)
-
-/* offsets from pub/priv config space */
-#define TXTCR_SINIT_BASE            0x0270
-#define TXTCR_SINIT_SIZE            0x0278
-#define TXTCR_HEAP_BASE             0x0300
-#define TXTCR_HEAP_SIZE             0x0308
 
 #define SHA1_SIZE      20
 typedef uint8_t   sha1_hash_t[SHA1_SIZE];
@@ -411,7 +395,7 @@ int __init tboot_protect_mem_regions(void)
 
     /* TXT Private Space */
     rc = e820_change_range_type(&e820, TXT_PRIV_CONFIG_REGS_BASE,
-                 TXT_PRIV_CONFIG_REGS_BASE + NR_TXT_CONFIG_PAGES * PAGE_SIZE,
+                 TXT_PRIV_CONFIG_REGS_BASE + TXT_CONFIG_SPACE_SIZE,
                  E820_RESERVED, E820_UNUSABLE);
     if ( !rc )
         return 0;
