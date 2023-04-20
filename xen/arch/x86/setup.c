8a496bec884af2843e937b197e846ce65ecd6cd6
Krystian Hebel <krystian.hebel@3mdeb.com>
2023-04-20 16:21:03 +0200

    x86/setup.c: don't use XSM policy and ucode updates with slaunch
    These must be measured before use, and code for choosing proper module
    does partial reads of all modules until it finds a matching one. This
    may introduce TOCTOU issues, so until introduction of secdev driver
    those functionalities are temporarily disabled when Xen is started
    with slaunch enabled.
    
    Signed-off-by: Krystian Hebel <krystian.hebel@3mdeb.com>

--- xen/arch/x86/setup.c
+++ xen/arch/x86/setup.c
@@ -1029,6 +1029,13 @@ void __init noreturn __start_xen(unsigned long mbi_p)
                mbi->mods_count);
     }
 
+    if ( sl_status )
+        if ( mbi->mods_count > 2 )
+        {
+            mbi->mods_count = 2;
+            printk("Excessive multiboot modules for slaunch - limiting to 2\n");
+        }
+
     bitmap_fill(module_map, mbi->mods_count);
     __clear_bit(0, module_map); /* Dom0 kernel is always first */
 
@@ -1710,7 +1717,9 @@ void __init noreturn __start_xen(unsigned long mbi_p)
     mmio_ro_ranges = rangeset_new(NULL, "r/o mmio ranges",
                                   RANGESETF_prettyprint_hex);
 
-    xsm_multiboot_init(module_map, mbi);
+    /* TODO: XSM policies are not supported by slaunch yet. */
+    if ( sl_status == 0 )
+        xsm_multiboot_init(module_map, mbi);
 
     /*
      * IOMMU-related ACPI table parsing may require some of the system domains
@@ -1779,7 +1788,9 @@ void __init noreturn __start_xen(unsigned long mbi_p)
 
     init_IRQ();
 
-    microcode_grab_module(module_map, mbi);
+    /* TODO: Microcode updates are not supported by slaunch yet. */
+    if ( sl_status == 0 )
+        microcode_grab_module(module_map, mbi);
 
     timer_init();
 
