180f2caeae331c67d8dcf01bbe932f1ab1abb543
Sergii Dmytruk <sergii.dmytruk@3mdeb.com>
2024-03-17 00:58:26 +0200

    arch/x86: support slaunch with AMD SKINIT
    This mostly involves not running Intel-specific code when on AMD.
    
    There are only a few new AMD-specific implementation details:
     - finding SLB start and size and then mapping and protecting it
     - managing offset for adding the next TPM log entry (TXT-compatible
       data prepared by SKL is stored inside of vendor data field of TCG
       header)
    
    Signed-off-by: Sergii Dmytruk <sergii.dmytruk@3mdeb.com>
    Signed-off-by: Krystian Hebel <krystian.hebel@3mdeb.com>

--- xen/arch/x86/tpm.c
+++ xen/arch/x86/tpm.c
@@ -31,6 +31,7 @@ asm (
 #include "include/asm/intel_txt.h"
 #include "include/asm/slaunch.h"
 #include "include/asm/tpm.h"
+#include "include/asm/x86-vendors.h"
 #ifdef __va
 #error "__va defined in non-paged mode!"
 #endif
@@ -63,14 +64,35 @@ void *memcpy(void *dest, const void *src, size_t n)
     return dest;
 }
 
+static bool is_amd_cpu(void)
+{
+    /*
+     * asm/processor.h can't be included in early code, which means neither
+     * cpuid() function nor boot_cpu_data can be used here.
+     */
+    uint32_t eax, ebx, ecx, edx;
+    asm volatile ( "cpuid"
+          : "=a" (eax), "=b" (ebx), "=c" (ecx), "=d" (edx)
+          : "0" (0), "c" (0) );
+    return ebx == X86_VENDOR_AMD_EBX
+        && ecx == X86_VENDOR_AMD_ECX
+        && edx == X86_VENDOR_AMD_EDX;
+}
+
 #else   /* __EARLY_TPM__ */
 
 #include <xen/mm.h>
 #include <xen/pfn.h>
 #include <xen/types.h>
 #include <asm/intel_txt.h>
+#include <asm/processor.h>
 #include <asm/slaunch.h>
 #include <asm/tpm.h>
+
+static bool is_amd_cpu(void)
+{
+    return boot_cpu_data.x86_vendor == X86_VENDOR_AMD;
+}
 
 #endif  /* __EARLY_TPM__ */
 
@@ -260,6 +282,21 @@ struct TPM12_PCREvent {
     uint8_t Data[];
 };
 
+struct tpm1_spec_id_event {
+    uint32_t pcrIndex;
+    uint32_t eventType;
+    uint8_t digest[20];
+    uint32_t eventSize;
+    uint8_t signature[16];
+    uint32_t platformClass;
+    uint8_t specVersionMinor;
+    uint8_t specVersionMajor;
+    uint8_t specErrata;
+    uint8_t uintnSize;
+    uint8_t vendorInfoSize;
+    uint8_t vendorInfo[0];  /* variable number of members */
+} __packed;
+
 struct txt_ev_log_container_12 {
     char        Signature[20];      /* "TXT Event Container", null-terminated */
     uint8_t     Reserved[12];
@@ -401,6 +438,15 @@ static void *create_log_event12(struct txt_ev_log_container_12 *evt_log,
                                 unsigned data_size)
 {
     struct TPM12_PCREvent *new_entry;
+
+    if ( is_amd_cpu() ) {
+        /*
+         * On AMD, TXT-compatible structure is stored as vendor data of
+         * TCG-defined event log header.
+         */
+        struct tpm1_spec_id_event *spec_id = (void *)evt_log;
+        evt_log = (struct txt_ev_log_container_12 *)&spec_id->vendorInfo[0];
+    }
 
     new_entry = (void *)(((uint8_t *)evt_log) + evt_log->NextEventOffset);
 
@@ -837,10 +883,27 @@ static uint32_t tpm2_hash_extend(unsigned loc, uint8_t *buf, unsigned size,
 
 #endif /* __EARLY_TPM__ */
 
-static struct heap_event_log_pointer_element2_1 *find_evt_log_ext_data(void)
+static struct heap_event_log_pointer_element2_1 *
+find_evt_log_ext_data(struct tpm2_spec_id_event *evt_log)
 {
     struct txt_os_sinit_data *os_sinit;
     struct txt_ext_data_element *ext_data;
+
+    if ( is_amd_cpu() ) {
+        /*
+         * Event log pointer is defined by TXT specification, but
+         * secure-kernel-loader provides a compatible structure in vendor data
+         * of the log.
+         */
+        const uint8_t *data_size =
+            (void *)&evt_log->digestSizes[evt_log->digestCount];
+
+        if ( *data_size != sizeof(struct heap_event_log_pointer_element2_1) )
+            return NULL;
+
+        /* Vendor data directly follows one-byte size. */
+        return (void *)(data_size + 1);
+    }
 
     os_sinit = txt_os_sinit_data_start(__va(read_txt_reg(TXTCR_HEAP_BASE)));
     ext_data = (void *)((uint8_t *)os_sinit + sizeof(*os_sinit));
@@ -874,7 +937,7 @@ create_log_event20(struct tpm2_spec_id_event *evt_log, uint32_t evt_log_size,
     unsigned i;
     uint8_t *p;
 
-    log_ext_data = find_evt_log_ext_data();
+    log_ext_data = find_evt_log_ext_data(evt_log);
     if ( log_ext_data == NULL )
         return log_hashes;
 
