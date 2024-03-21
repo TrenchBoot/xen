95aa54c4531de7f927bef85a95d6db174a02ecb1
Sergii Dmytruk <sergii.dmytruk@3mdeb.com>
2024-03-21 19:41:06 +0200

    x86/boot: find MBI and SLRT on AMD
    secure-kernel-loader on AMD with SKINIT passes MBI as a parameter for
    Multiboot kernel.
    
    Another thing of interest is the location of SLRT which is bootloader's
    data after SKL.
    
    Signed-off-by: Sergii Dmytruk <sergii.dmytruk@3mdeb.com>

--- xen/arch/x86/boot/slaunch_early.c
+++ xen/arch/x86/boot/slaunch_early.c
@@ -20,7 +20,8 @@
  * - %eax       = load_base_addr,
  * - %edx       = tgt_base_addr,
  * - %ecx       = tgt_end_addr,
- * - 0x04(%esp) = &result.
+ * - 0x04(%esp) = slaunch_param,
+ * - 0x08(%esp) = &result.
  */
 asm (
     "    .text                         \n"
@@ -32,12 +33,29 @@ asm (
 #include <xen/macros.h>
 #include <xen/types.h>
 #include <asm/intel_txt.h>
+#include <xen/slr_table.h>
+#include <asm/x86-vendors.h>
 
 struct early_tests_results
 {
     uint32_t mbi_pa;
     uint32_t slrt_pa;
 } __packed;
+
+static bool is_intel_cpu(void)
+{
+    /*
+     * asm/processor.h can't be included in early code, which means neither
+     * cpuid() function nor boot_cpu_data can be used here.
+     */
+    uint32_t eax, ebx, ecx, edx;
+    asm volatile ( "cpuid"
+          : "=a" (eax), "=b" (ebx), "=c" (ecx), "=d" (edx)
+          : "0" (0), "c" (0) );
+    return ebx == X86_VENDOR_INTEL_EBX
+        && ecx == X86_VENDOR_INTEL_ECX
+        && edx == X86_VENDOR_INTEL_EDX;
+}
 
 static void verify_pmr_ranges(struct txt_os_mle_data *os_mle,
                               struct txt_os_sinit_data *os_sinit,
@@ -118,12 +136,42 @@ static void verify_pmr_ranges(struct txt_os_mle_data *os_mle,
 void slaunch_early_tests(uint32_t load_base_addr,
                          uint32_t tgt_base_addr,
                          uint32_t tgt_end_addr,
+                         uint32_t slaunch_param,
                          struct early_tests_results *result)
 {
     void *txt_heap;
     struct txt_os_mle_data *os_mle;
     struct txt_os_sinit_data *os_sinit;
     uint32_t size = tgt_end_addr - tgt_base_addr;
+
+    if ( !is_intel_cpu() )
+    {
+        /*
+         * Not an Intel CPU. Currently the only other option is AMD with SKINIT
+         * and secure-kernel-loader.
+         */
+        struct slr_table *slrt;
+        struct slr_entry_dl_info *dl_info;
+
+        const uint16_t *sl_header = (void *)slaunch_param;
+        /*
+         * The fourth 16-bit integer of SKL's header is an offset to
+         * bootloader's data, which is SLRT.
+         */
+        result->slrt_pa = slaunch_param + sl_header[3];
+        slrt = (struct slr_table *)result->slrt_pa;
+
+        result->mbi_pa = 0;
+        dl_info = (struct slr_entry_dl_info *)
+                  slr_next_entry_by_tag (slrt, NULL, SLR_ENTRY_DL_INFO);
+        /* Basic checks only, SKL checked and consumed the rest. */
+        if ( dl_info                             == NULL
+             || dl_info->hdr.size                != sizeof(*dl_info)
+             || dl_info->bl_context.bootloader   != SLR_BOOTLOADER_GRUB )
+            result->mbi_pa = dl_info->bl_context.context;
+
+        return;
+    }
 
     /* Clear the TXT error registers for a clean start of day */
     write_txt_reg(TXTCR_ERRORCODE, 0);
